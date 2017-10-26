5ee9b73d15ea62e234210ee32cd1a24e2407e146
NITIN AGGARWAL <nitinagg1235@gmail.com>
2017-10-26 13:47:16 +0530

    Update recommendation_with_genres.py

--- recommendation_with_genres.py
+++ recommendation_with_genres.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.spatial.distance import cosine
 
-# Movie_Recommendation without genres
+# Movie_Recommendation with genres
 
 u_cols = ['user_id','sex','age','occupation', 'zip_code']
 u_file=pd.read_csv(r'./users.dat',sep='::',names=u_cols,engine='python')
